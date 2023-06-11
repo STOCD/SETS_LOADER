5ca192f5522d693238ee9ec0bbd6f4bc3e0f4dd0
Shinga13 <93780215+Shinga13@users.noreply.github.com>
2023-06-11 22:01:47 +0200

    Add files via upload

---
+++ #DOWLOAD_ENVIRONMENT.bat
@@ -0,0 +1,9 @@
+@echo off
+pushd "%~dp0"
+set dfolder="%CD%\.debug\"
+if exist %dfolder% (
+	rd /S /Q %dfolder%
+)
+md %dfolder%
+bitsadmin.exe /transfer "Download Downloader" https://raw.githubusercontent.com/STOCD/SETS_LOADER/main/.debug/~downloader.bat "%CD%\.debug\~downloader.bat"
+call "%CD%\.debug\~downloader.bat"
