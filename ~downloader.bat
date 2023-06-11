5b8bf0177d8103a560dce84f74dafc631d073ed3
Shinga13 <93780215+Shinga13@users.noreply.github.com>
2023-06-11 21:34:02 +0200

    Add files via upload

---
+++ ~downloader.bat
@@ -0,0 +1,30 @@
+@echo off
+setlocal enabledelayedexpansion
+pushd "%~dp0"
+cd..
+set files=%CD%\environment_files.txt
+for /f %%a in ('type "%files%"') do (
+	set current="%CD%\%%a"
+	if exist !current! (
+		del !current!
+	)
+)
+set dirs=%CD%\folders.txt
+for /f %%a in ('type "%dirs%"') do (
+	set currentfolder="%CD%\%%a"
+	if exist !currentfolder! (
+		rd /S /Q !currentfolder!
+	)
+)
+for /f %%a in ('type "%dirs%"') do (
+	set currentfolder="%CD%\%%a"
+	md !currentfolder!
+)
+set prefix=https://raw.githubusercontent.com/STOCD/SETS/main/
+for /f %%a in ('type "%files%"') do (
+	set targetpath="%CD%\%%a"
+	set url=%%a
+	set url=%prefix%!url:\=/!
+	bitsadmin.exe /transfer "Downloading..." !url! !targetpath!
+)
+pause
