0fc3354e78199545c4ea2b8b1c2befec5f3e9941
Shinga13 <93780215+Shinga13@users.noreply.github.com>
2023-06-11 22:05:48 +0200

    Add files via upload

--- RUN.bat
+++ RUN.bat
@@ -1,2 +1,4 @@
 pushd "%~dp0"
+bitsadmin.exe /transfer "DownloadCurrentVersion" https://raw.githubusercontent.com/STOCD/SETS/main/main.py "%CD%\main.py"
+echo [%date%]-[%time%] -- File download complete! >> "%CD%\debug\log.txt"
 python "%CD%\main.py"
