e32bdbb8b0c7e8ca4a7aa36997e6d7c6505728ee
Shinga13 <93780215+Shinga13@users.noreply.github.com>
2023-06-12 10:15:56 +0200

    Update and rename #RUN-SNAPSHOT.bat to #RUN-SNAPSHOT.bat

--- .debug/#RUN-SNAPSHOT.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-pushd "%~dp0"
-cd..
-python "%CD%\src\PreApp\snapshot.py" >> "%CD%\.debug\snapshot-output.txt"
-echo [%date%]-[%time%] -- Snapshot complete! >> "%CD%\.debug\log.txt"
-echo [%date%]-[%time%] -- Snapshot complete!
-pause
