a069ae970bcf01bb183d33db25562fe68780b9d0
Shinga13 <93780215+Shinga13@users.noreply.github.com>
2023-06-12 07:53:42 +0200

    Add files via upload

--- .debug/#SETUP.bat
+++ .debug/#SETUP.bat
@@ -1,9 +1,12 @@
+@echo off
 pushd "%~dp0"
 python "pip.py"
 cd..
 set setsdir=%CD%
 pushd %userprofile%
-python -m pip install -r %setsdir%\requirements.txt
+@echo on
+python -m pip install -r "%setsdir%\requirements.txt"
+@echo off
 pushd "%~dp0"
 echo [%date%]-[%time%] -- Requirements installed! >> log.txt
 echo [%date%]-[%time%] -- Requirements installed!
