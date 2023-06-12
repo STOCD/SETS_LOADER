ea3eda64678dd731a50b9485f1272107fc4138f6
Shinga13 <93780215+Shinga13@users.noreply.github.com>
2023-06-12 08:45:43 +0200

    Add files via upload

---
+++ #DOWNLOAD_APP_FILES.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal enabledelayedexpansion
+pushd "%~dp0"
+set files=%CD%\.debug\~app_files.txt
+set prefix=https://raw.githubusercontent.com/STOCD/SETS/main/
+for /f %%a in ('type "%files%"') do (
+	set targetpath="%CD%\%%a"
+	set url=%%a
+	set url=%prefix%!url:\=/!
+	bitsadmin.exe /transfer "Downloading..." !url! !targetpath!
+)
+cls
+echo [%date%]-[%time] -- App files successfully downloaded >> "%CD%\.debug\log.txt
+echo [%date%]-[%time] -- App files successfully downloaded
+pause
