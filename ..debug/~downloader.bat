2037af71c147de250c64a0c606496170677d77aa
Shinga13 <93780215+Shinga13@users.noreply.github.com>
2023-08-04 07:50:17 +0200

    Update ~downloader.bat

--- ..debug/~downloader.bat
+++ ..debug/~downloader.bat
@@ -18,6 +18,9 @@ bitsadmin.exe /transfer "Download#UNINSTALL-REQUIREMENTS" https://raw.githubuser
 bitsadmin.exe /transfer "Download#DOWNLOAD_APP_FILES" https://raw.githubusercontent.com/STOCD/SETS_LOADER/main/%%23DOWNLOAD_APP_FILES.bat "%CD%\#DOWNLOAD_APP_FILES.bat"
 bitsadmin.exe /transfer "DownloadReadme" https://raw.githubusercontent.com/STOCD/SETS_LOADER/main/README.md "%CD%\README.txt"
 bitsadmin.exe /transfer "Download.debug" https://raw.githubusercontent.com/STOCD/SETS_LOADER/main/..debug/.debug "%CD%\..debug\.debug"
+if exist %CD%\.config\images\ (
+	move /y %CD%\.config\images\ %CD%
+)
 for /f %%a in ('type "%files%"') do (
 	set current="%CD%\%%a"
 	if exist !current! (
@@ -33,6 +36,11 @@ for /f %%a in ('type "%dirs%"') do (
 for /f %%a in ('type "%dirs%"') do (
 	set currentfolder="%CD%\%%a"
 	md !currentfolder!
+)
+if exist %CD%\images\ (
+	if exist %CD%\.config\ (
+		move /y %CD%\images\ %CD%\.config\
+	)
 )
 set prefix=https://raw.githubusercontent.com/STOCD/SETS/main/
 for /f %%a in ('type "%files%"') do (
